6a605e8a5c50187f26eb9d6b4808f9307d093c0f
Denis <denisvovchenko96@mail.ru>
2019-03-31 17:43:12 +0200

    complete desktop version (almost)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,66 @@
 const show = value => console.log(value);
+let html = document.documentElement;
+let body = document.querySelector('body');
+
+// Navigation
+{
+  
+}
+
+// Delete modal popup
+const removePopup = popup => {
+  body.removeChild(popup);
+  body.classList.remove('modal-open');
+}
+
+// Create modal popup
+const createPopup = block => {
+  let popupOverlay = document.createElement('div');
+  popupOverlay.classList = 'modal__overlay';  
+  popupOverlay.addEventListener('click', e => {
+    e.preventDefault();
+
+    let target = e.target;
+
+    if (!target.classList.contains('modal__overlay')) return;
+
+    removePopup(popupOverlay);
+  }, true);
+
+  let popupWindow = document.createElement('div');
+  popupWindow.classList = 'modal';
+  popupOverlay.appendChild(popupWindow);
+
+  let closeBtn = document.createElement('button');
+  closeBtn.classList = 'close-btn modal__close-btn';
+  closeBtn.setAttribute('type', 'button');
+
+  let btnAllyContent = document.createElement('span');
+  btnAllyContent.classList = 'visually-hidden';
+  btnAllyContent.innerHTML = 'Zavřít';
+  closeBtn.appendChild(btnAllyContent);
+  closeBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    removePopup(popupOverlay);
+  });
+  
+  let popupInner = block;
+
+  popupWindow.appendChild(popupInner);
+  popupWindow.appendChild(closeBtn);
+  body.appendChild(popupOverlay);
+  body.classList.add('modal-open');
+
+  // Fix in the future!!!
+  popupOverlay.style.display = 'flex';
+
+  document.addEventListener('keydown', e => {
+    if (e.keyCode === 27 && body.classList.contains('modal-open')) {
+      removePopup(popupOverlay);
+    }
+  });
+}
 
 // Gallery slider
 
@@ -59,89 +121,254 @@ window.addEventListener('resize', () => {
 });
 
 // Gallery popup
+{
+  const imagePopup = () => {
 
-const imagePopup = () => {
-  let imageLinks = document.querySelectorAll('.gallery__link');
-  let modalOverlay = document.querySelector('.modal__overlay');
-  let modal = document.querySelector('.gallery__modal');
-  let currentElement;
-  let modalImage = modal.querySelector('.modal__img');
+    let galleryModalBlock = document.createElement('div');
+    galleryModalBlock.classList = 'gallery-popup__block';
 
-  imageLinks.forEach(link => {
+    let modalPic = document.createElement('picture');
+    modalPic.classList = 'gallery-popup__pic';
+    galleryModalBlock.appendChild(modalPic);
+
+    let modalSource = document.createElement('source');
+    modalSource.srcset = '';
+    modalSource.type = 'image/webp';
+    modalPic.appendChild(modalSource);
+
+    let modalImage = document.createElement('img');
+    modalImage.classList = 'gallery-popup__img';
+    modalPic.appendChild(modalImage);
+
+    let modalControls = document.createElement('div');
+    modalControls.classList = 'modal__controls controls';
+    galleryModalBlock.appendChild(modalControls);
+
+    let modalPrevBtn = document.createElement('button');
+    modalPrevBtn.classList = 'modal__btn modal__btn--prev controls__btn controls__btn--prev';
+    modalPrevBtn.type = 'button';
+    modalPrevBtn.innerHTML = '<span class="visually-hidden">Předchozí</span>';
+    modalControls.appendChild(modalPrevBtn);
+
+    let modalNextBtn = document.createElement('button');
+    modalNextBtn.classList = 'modal__btn modal__btn--next controls__btn controls__btn--next';
+    modalNextBtn.type = 'button';
+    modalNextBtn.innerHTML = '<span class="visually-hidden">Dálší</span>';
+    modalControls.appendChild(modalNextBtn);
+
+    let imageLinks = document.querySelectorAll('.gallery__link');
+    let currentElement;
+
+    imageLinks.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+
+        currentElement = link.closest('.gallery__item');
+        let imageSrc = link.href;
+        let imageWebpSrc = link.querySelector('source').srcset;    
+        modalImage.src = imageSrc;
+        modalSource.src = imageWebpSrc;
+
+        createPopup(galleryModalBlock);
+
+        document.querySelectorAll('.modal__btn').forEach(function(btn) {
+          btn.addEventListener('click', function(e) {
+            movePopupPhotos(e);
+          });
+        });
+
+      });
+    });
+
+    // Switch photo in popup
+
+    const movePopupPhotos = e => {
+      let target = e.target;
+
+      if (target.classList.contains('modal__btn--prev') || e.keyCode === 37) {
+
+        if (!currentElement.previousElementSibling) {
+          currentElement = currentElement.parentElement.lastElementChild;
+        }
+
+        currentElement = currentElement.previousElementSibling;
+
+        modalImage.src = currentElement.querySelector('.gallery__link').href;
+
+      } else if (target.classList.contains('modal__btn--next') || e.keyCode === 39) {
+
+        if (!currentElement.nextElementSibling) {
+          currentElement = currentElement.parentElement.firstElementChild;
+        }
+
+        currentElement = currentElement.nextElementSibling;
+
+        modalImage.src = currentElement.querySelector('.gallery__link').href;
+      }
+    }    
+
+    document.addEventListener('keydown', e => {
+      let galleryExists = document.querySelector('.gallery-popup__block');
+
+      if ((e.keyCode != 37 && e.keyCode != 39) || !galleryExists) return;
+
+      movePopupPhotos(e);
+    });
+
+  }
+
+  imagePopup();
+}
+
+// Slide toggle bio
+{
+  let open = false;
+  let heightChecked = false;
+  let initHeight = 0;  
+
+  const slideToggle = () => {
+    if (!open) {
+      open = true;
+      for (let i = 0; i < bioAddHeight; i++) {
+        bioAdd.style.height = `${i}px`;
+      }
+
+    } else {
+      open = false;
+      for (let i = bioAddHeight; i >= 0; i--) {
+        bioAdd.style.height = `${i}px`;
+      }
+    }    
+  }
+
+  let bioBtnSlide = document.querySelector('.bio__btn--slide');
+  let bioAdd = document.querySelector('.bio__add-text');
+  let bioAddHeight = bioAdd.offsetHeight;
+  bioAdd.style.height = 0;
+
+  bioBtnSlide.addEventListener('click', function() {
+    slideToggle(bioAdd);
+  });
+}
+
+// Popup bio
+{
+  let openPopupBtn = document.querySelector('.bio__btn--modal');
+  let bioTitle = document.querySelector('.bio__title').cloneNode(true);
+  let bioTexts = document.querySelectorAll('.bio__text');
+  let popupBlock = document.createElement('div');
+  popupBlock.classList = 'bio-popup__block';
+
+  let popupBody = document.createElement('div');
+  popupBody.classList = 'bio-popup__body';
+  
+  popupBlock.appendChild(bioTitle);
+  popupBlock.appendChild(popupBody);
+
+  bioTexts.forEach(text => {
+    let cloneText = text.cloneNode(true);
+    popupBody.appendChild(cloneText);
+  });
+  
+  openPopupBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    createPopup(popupBlock);
+  });
+
+  // const closeModal = () => {
+  //   let closeBtn = document.querySelector('.modal__close-btn');
+
+  //   document.addEventListener('click', function(e) {
+  //     let target = e.target;
+
+  //     if (target !== bioModal && target !== closeBtn) return;
+
+  //     bioModal.classList.remove('modal__overlay--show');
+  //     });
+
+  //     window.addEventListener('keydown', e => {
+  //     if (e.keyCode === 27) {
+  //       bioModal.classList.remove('modal__overlay--show');
+  //     }
+  //   });
+  // }
+
+  // closeModal();
+}
+
+// Popup cd
+{
+  let cdBlock = document.querySelector('.cd');
+  let cdLinks = cdBlock.querySelectorAll('.cd__link');
+  
+  cdLinks.forEach((link) => {
     link.addEventListener('click', e => {
       e.preventDefault();
 
-      currentElement = link.closest('.gallery__item');
-      show(currentElement);
-      let imageSrc = link.href;
-      modalOverlay.classList.add('modal__overlay--show');      
-      modalImage.src = imageSrc;
-    });
-  });
+      let cdPopupBlock = document.createElement('div');
+      cdPopupBlock.classList = 'cd-popup__block';
 
-  // Close modal 
-
-  const closeModal = () => {
-    let closeBtn = document.querySelector('.modal__close-btn');
-    let modalOverlay = document.querySelector('.modal__overlay');
-
-    document.addEventListener('click', function(e) {
-      let target = e.target;
-
-      if (target !== modalOverlay && target !== closeBtn) return;
-
-      modalOverlay.classList.remove('modal__overlay--show');
-    });
-
-    window.addEventListener('keydown', e => {
-      if (e.keyCode === 27) {
-        modalOverlay.classList.remove('modal__overlay--show');
-      }
-    });
-  }
-
-  closeModal();
-
-  // Switch photo in popup
-
-  const movePopupPhotos = (e) => {
-    let target = e.target;
+      for (let i = 0; i < link.children.length; i++) {
+        let node = link.children[i].cloneNode(true);
+        cdPopupBlock.appendChild(node);
+      };
       
-    if (target.classList.contains('modal__btn--prev') || e.keyCode === 37) {
-      
-      if (!currentElement.previousElementSibling) {
-        currentElement = currentElement.parentElement.lastElementChild;
-      }
-
-      currentElement = currentElement.previousElementSibling;
-
-      modalImage.src = currentElement.querySelector('.gallery__link').href;
-
-    } else if (target.classList.contains('modal__btn--next') || e.keyCode === 39) {
-
-      if (!currentElement.nextElementSibling) {
-        currentElement = currentElement.parentElement.firstElementChild;
-      }
-
-      currentElement = currentElement.nextElementSibling;
-
-      modalImage.src = currentElement.querySelector('.gallery__link').href;
-    }
-  }
-
-  document.querySelectorAll('.modal__btn').forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-      movePopupPhotos(e);
+      createPopup(cdPopupBlock);
     });
-  });
+  })
 
-  document.addEventListener('keydown', e => {
-    movePopupPhotos(e);
-  });
-  
 }
 
-imagePopup();
+// Popup pieces
+{
+  let piecesBlock = document.querySelector('.pieces');
+  let piecesBtn = piecesBlock.querySelector('.pieces__btn');
+  piecesBtn.addEventListener('click', e => {
+    e.preventDefault();
 
+    let node = piecesBlock.querySelector('.container').cloneNode(true);
+    node.classList = 'pieces-popup__block';
 
+    createPopup(node);
+  });
+}
 
+// Youtube videos
+{
+  const setVideos = () => {
+    let videos = document.querySelectorAll('.video__item');
+    
+
+    videos.forEach(video => {
+      video.addEventListener('click', e => {
+        e.preventDefault();
+
+        let link = video.querySelector('.video__link');
+        let img = video.querySelector('.video__img');
+        let btn = video.querySelector('.video__btn');
+
+        let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)default\.jpg/i;
+        let url = img.src;
+        let match = url.match(regexp);
+        show(url);
+        let videoId = match[1];
+
+        let query = '?rel=0&showinfo=0&autoplay=1';
+        let videoUrl = `https://www.youtube.com/embed/${videoId}/${query}`;
+        
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('src', videoUrl);
+        iframe.classList = 'video__iframe';
+
+        video.removeChild(link);
+        video.removeChild(btn);
+        video.appendChild(iframe);
+      });
+    });
+  }
+
+  setVideos();
+
+}
